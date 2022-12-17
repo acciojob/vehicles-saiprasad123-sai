b0e4616a60e49be5a46246b1bd8aff64450b1421
sai prasad <kondaparthisaiprasad117@gmail.com>
2022-12-17 13:33:36 +0530

    commit1

--- src/main/java/com/driver/Car.java
+++ src/main/java/com/driver/Car.java
@@ -10,16 +10,35 @@ public class Car extends Vehicle {
     private int seats;
 
     public Car(String name, int wheels, int doors, int gears, boolean isManual, String type, int seats) {
+        super();
+        this.wheels = wheels;
+        this.isManual = isManual;
+        this.doors = doors;
+        this.gears = gears;
+        this.type = type;
+        this.seats = seats;
+        this.type = type;
         //Hint: Car extends Vehicle
+
+
+    }
+
+    public Car(String name, boolean isManual) {
+        super();
+        this.isManual = isManual;
     }
 
     public void changeGear(int newGear){
-
+        this.currentGear = newGear;
         System.out.println("changeGear method called - The gear is changed to: " + currentGear);
     }
 
     public void changeSpeed(int newSpeed, int newDirection){
-
+        super.move(newSpeed,newDirection);
         System.out.println("changeSpeed method called - The speed is changed to: " + newSpeed + ", and the direction is changed to: " + newDirection + " degrees");
+    }
+
+    public int getcurrentDirection() {
+        return super.getCurrentDirection();
     }
 }
--- src/main/java/com/driver/F1.java
+++ src/main/java/com/driver/F1.java
@@ -3,11 +3,12 @@ package com.driver;
 public class F1 extends Car {
 
     public F1(String name, boolean isManual) {
+        super(name,isManual);
         //Use arbitrary values for parameters which are not mentioned
     }
 
     public void accelerate(int rate){
-        int newSpeed = 0; //set the value of new speed by using currentSpeed and rate
+        int newSpeed = this.getCurrentSpeed()+rate; //set the value of new speed by using currentSpeed and rate
         /**
          * speed 0: gear 1
          * speed 1-50: gear 1
@@ -18,13 +19,30 @@ public class F1 extends Car {
          * speed more than 250: gear 6
          */
 
-        if(newSpeed == 0) {
+        if(newSpeed == 0 || newSpeed>0 && newSpeed<=50) {
             //Stop the car, set gear as 1
+            this.changeGear(1);
         }
+        else if(newSpeed>50 && newSpeed<=100)
+            this.changeGear(2);
+
+        else if(newSpeed>100 && newSpeed<=150)
+            this.changeGear(3);
+
+        else if(newSpeed>150 && newSpeed<=200)
+            this.changeGear(4);
+
+        else if(newSpeed>200 && newSpeed<=250)
+            this.changeGear(5);
+        else this.changeGear(6);
         //for all other cases, change the gear accordingly
 
         if(newSpeed > 0) {
             changeSpeed(newSpeed, getCurrentDirection());
         }
+    }
+
+    public int getCurrentDirection() {
+        return super.getcurrentDirection();
     }
 }
