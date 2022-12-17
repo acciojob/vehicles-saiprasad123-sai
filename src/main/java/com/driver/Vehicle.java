c71b824f5dc7fe860b90af33a3f6996e93bb6007
sai prasad <kondaparthisaiprasad117@gmail.com>
2022-12-17 15:26:32 +0530

    m10

--- src/main/java/com/driver/Vehicle.java
+++ src/main/java/com/driver/Vehicle.java
@@ -5,6 +5,10 @@ public class Vehicle {
     private String name;
     private int currentSpeed;
     private int currentDirection;
+
+    public Vehicle(){
+
+    }
     public Vehicle(String name){
         this.name=name;
     }
